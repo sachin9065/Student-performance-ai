9533dca873a3ce5d9a3f8feeca46173f63caa4cb
Sachin Singh <sachin271100@gmail.com>
2025-09-26 08:22:06 +0000

    jo first page es app ka usko dark theme me kr do

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
